cf1b0178e6857b9c9b505671cc9899744cad7d77
Oli <dev.olivercarlos@gmail.com>
2020-04-18 22:28:08 +0800

    Use api service to load out attendance spreadsheet.

--- src/services/AttendanceService.js
+++ src/services/AttendanceService.js
@@ -1,8 +1,23 @@
 import apiService from './ApiService';
+import GoogleSpreadsheetService from './GoogleSpreadsheetService';
 
+/**
+ * This will get the attendances basing on the
+ * serverId that is provided by the request.
+ * @param {string} serverId
+ */
+export function get(serverId) {
+  return apiService.get('/attendances', {
+    params: { serverId },
+  }).then((response) => {
+    const { data } = response;
+    return GoogleSpreadsheetService.loadSpreadsheet(data.spreadsheet_id)
+      .then((res) => {
 
-export function get() {
-  apiService.get();
+      });
+  }).catch((error) => {
+    log({ error });
+  });
 }
 
 export function store() {
