7c12e035c59f61080b46539ef1ce656b668f3280
Oli <dev.olivercarlos@gmail.com>
2020-06-02 00:18:37 +0800

    WIP: help command to display all commands available.

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,11 +1,21 @@
+const prefix = process.env.PREFIX;
+const owner = process.env.OWNER;
+
 module.exports.run = async (client, msg) => {
-  await msg.channel.send('This is help. WIP.');
+  const { commands } = client;
+  if (!commands.length) {
+    for (command in commands) {
+      //TODO: Loop up and display all the available commands.
+    }
+  } else {
+    await msg.channel.send(`My master, <@${owner}>, has not created any commands for me yet.`);
+  }
 };
 
 module.exports.help = {
   name: 'help',
-  description: '',
-  usage: '',
+  description: 'List all of my commands or info about a specific command.',
+  usage: 'List all of my commands or info about a specific command.',
   category: 'Misc',
-  aliases: [''],
+  aliases: ['commands'],
 };
